e91afb50c02a07a1c8067c95371f3fa4f30503ec
404 <69495305+Phil504@users.noreply.github.com>
2023-07-04 10:07:53 +0900

    solving

--- 2563.cpp
+++ 2563.cpp
@@ -7,9 +7,32 @@
 
 using namespace std;
 
-vector<pair<int, int>> list;
 int main(void){
-    
+    bool map[101][101] = {false};
+    int x1, y1, paper, biggest_x = 0, biggest_y = 0, result = 0;
+
+    cin >> paper;
+
+    for(int i = 0; i < paper; i++){
+        cin >> x1 >> y1;
+
+        if(biggest_x < (x1+10)) biggest_x = x1+10;
+        if(biggest_y < (y1+10)) biggest_y = y1+10;
+
+        for(int x = x1; x < 10+x1; x++){
+            for(int y = y1; y < 10+y1; y++){
+                map[x][y] = true;
+            }
+        }
+    }
+
+    for(int x = 0; x < biggest_x; x++){
+        for(int y = 0; y < biggest_y; y++){
+            if(map[x][y]) result++;
+        }
+    }
+
+    cout << result << endl;
     
     return 0;
 }
--- 2669.cpp
+++ 2669.cpp
@@ -8,14 +8,30 @@
 using namespace std;
 
 int main(void){
-    int chart[101][101], square[4], number;
+    bool map[101][101] = {false};
+    int result = 0, biggest_x = 0, biggest_y = 0;
 
     for(int i = 0; i < 4; i++){
-        for(int j = 0; j < 4; j++){
-            cin >> number;
-            square[j] = number;
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        if(x2 > biggest_x) biggest_x = x2;
+        if(y2 > biggest_y) biggest_y = y2;
+
+        for(int x = x1; x < x2; x++){
+            for(int y = y1; y < y2; y++){
+                map[x][y] = true;
+            }
         }
     }
+
+    for(int x = 0; x <= biggest_x; x++){
+        for(int y = 0; y <= biggest_y; y++){
+            if(map[x][y]) result++;
+        }
+    }
+
+    cout << result << endl;
 
     return 0;
 }
